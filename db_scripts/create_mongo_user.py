41d98c8c2b71457e1bb87b8d0ee442c0a248dc33
Paulo Silva <paulo308@users.noreply.github.com>
2018-03-12 21:03:26 +0000

    Update create_mongo_user.py

--- db_scripts/create_mongo_user.py
+++ db_scripts/create_mongo_user.py
@@ -3,10 +3,10 @@ from pymongo import MongoClient
 _CODE_CREATE_USER = 
 db.getSiblingDB('$external').runCommand(
   {
-    createUser: 'OU=mongo_client,O=BIGSEA,L=Coimbra,ST=CB,C=PT',
+    createUser: 'OU=mongo_client,O=BIGSEA_client,L=Coimbra,ST=CB,C=PT',
     roles: [
              { role: 'readWrite', db: 'AAADB' },
-             { role: 'read', db: 'AAADB' }
+             { role: 'userAdminAnyDatabase', db: 'AAADB' }
            ],
     writeConcern: { w: 'majority' , wtimeout: 5000 }
   }
@@ -19,7 +19,7 @@ _DEFAULT_DB_PORT = 27017
 _DEFAULT_DB_NAME = 'AAADB'
 _DEFAULT_CLIENT_CERT = 'certs/mongo_client_crt.pem'
 _DEFAULT_CA_CERT = 'certs/root_ca.pem'
-_DEFAULT_USER = 'OU=mongo_client,O=BIGSEA,L=Coimbra,ST=CB,C=PT'
+_DEFAULT_USER = 'OU=mongo_client,O=BIGSEA_client,L=Coimbra,ST=CB,C=PT'
 _DEFAULT_MECHANISM = 'MONGODB-X509'
 
 if __name__ == '__main__':
